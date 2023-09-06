f8ea727a0d00343a605a477a889809c043ba506c
didimbote-joao <didimbote_joao@outlook.com>
2023-09-06 22:44:17 +0100

    add funcion clean

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function insert(num){
    var number = document.getElementById('resultado').innerHTML;
    document.getElementById('resultado').innerHTML= number + num;
 }
+
+function clean(){
+   document.getElementById('resultado').innerHTML ="";
+}
