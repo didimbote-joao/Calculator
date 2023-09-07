891b73730d4b4a0e3dcacbc0d9ae84a37d699069
didimbote-joao <didimbote_joao@outlook.com>
2023-09-07 10:07:25 +0100

    add function calculate

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ function back() {
    var result = document.getElementById('resultado').innerHTML;
    document.getElementById('resultado').innerHTML =result.substring(0, result.length -1)
 }
+
+function calculate() {
+   var result = document.getElementById('resultado').innerHTML;
+   if (result) {
+      document.getElementById('resultado').innerHTML = eval(result);
+   }
+   else{
+      document.getElementById('resultado').innerHTML ="";  
+   }
+}
